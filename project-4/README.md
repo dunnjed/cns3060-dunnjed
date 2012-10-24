066cb648297672542b18ecc3fde38fef5165aee2
Jed Dunn <dunnjed85@gmail.com>
2012-10-23 22:54:52 -0600

    Started README.md and updated simulation.cpp

---
+++ project-4/README.md
@@ -0,0 +1,25 @@
+Project4-Disk Scheduling
+=============
+
+Jed Dunn  
+
+I liked that you gave us the option to write this program in any language that we wanted to. I chose C++ simply because I've 
+never taken a class in C++ so I like to write programs in it whenever I get the chance so that I can continue to learn the 
+language. I liked that you chose this assignment because it definitely helped me to understand more fully how these disk
+scheduling algorithms work and why certain ones are better than others. You kind of get an idea of this from the slides,
+but writing this program made those ideas much clearer and better to understand.
+
+
+Summaries of Each Algorithm
+---------------------------
+
+###First Come First Served Algorithm###
+Content.
+
+
+###Shortest Seek Time First Algorithm###
+Content.
+
+
+###Scan-Look Algorithm###
+Content.
--- project-4/simulation.cpp
+++ project-4/simulation.cpp
@@ -6,8 +6,6 @@
 #include <vector>
 #include <algorithm>
 
-
-
 #define MAX_NUM_OF_TRACKS 100
 #define INITIAL_TRACK_POSITION 15
 #define PREV_TRACK_POSITION 12
@@ -15,6 +13,8 @@
 void firstComeFirstServed(int trackNums[], int numOfTracksEntered);
 void shortestSeekTimeFirst(int trackNums[], int numOfTracksEntered);
 void scanLook(int trackNums[], int numOfTracksEntered);
+void printStats(int currentTrack, int nextTrack, std::ostringstream &movement, int &totalTracksTraversed);
+
 
 int main(int argc, char *argv[])
 {
@@ -22,25 +22,13 @@ int main(int argc, char *argv[])
     int numOfTracksEntered = 0;
     int trackNums[MAX_NUM_OF_TRACKS];    
 
-
     std::cout << "Enter your track numbers:" << std::endl;
 
     int trackNum = -1;
-    //std::cin >> trackNum;
-    //int flag = 0;
-    while((std::cin >> trackNum) && trackNum > 0 && numOfTracksEntered < MAX_NUM_OF_TRACKS)
+    while((std::cin >> trackNum) && trackNum >= 0 && numOfTracksEntered < MAX_NUM_OF_TRACKS)
     {
         trackNums[numOfTracksEntered++] = trackNum;
-        //std::cout << " flag was: " << flag << std::endl;
-        //std::cin >> trackNum;
     }
-
-    //std::cout << std::endl;
-    
-    for(int i = 0; i < numOfTracksEntered; i++)
-        std::cout << trackNums[i] << std::endl;
-
-
 
     if(numOfTracksEntered > 0)
     {
@@ -51,45 +39,33 @@ int main(int argc, char *argv[])
         std::cout << std::endl;
         std::cout << std::endl;
         scanLook(trackNums, numOfTracksEntered);
-
+    }
+    else
+    {
+        std::cout << "Invalid input." << std::endl;
     }
 
-
-
-
     return 0;
-
 }
 
 void firstComeFirstServed(int trackNums[], int numOfTracksEntered)
 {
     const int fieldWidth = 30;
-    int tracksTraversed = 0;
     int totalTracksTraversed = 0;
+
     std::cout << "First Come First Served" << std::endl;
     std::cout << std::setiosflags(std::ios::left) << std::setw(fieldWidth) << "Head Movement" << "Tracks Traversed" << std::endl;
 
     std::ostringstream movement;
     
-    movement << "Tracks " << INITIAL_TRACK_POSITION << "-" << trackNums[0];
-    tracksTraversed = std::abs(INITIAL_TRACK_POSITION - trackNums[0]);
-    std::cout << std::setw(fieldWidth) << movement.str() << tracksTraversed << std::endl;
-    totalTracksTraversed += tracksTraversed;
-    movement.str("");
+    printStats(INITIAL_TRACK_POSITION, trackNums[0], movement, totalTracksTraversed);
 
     for(int i = 1; i < numOfTracksEntered; i++)
     {
-        movement << "Tracks " << trackNums[i-1] << "-" << trackNums[i];
-        tracksTraversed = std::abs(trackNums[i-1] - trackNums[i]);
-        std::cout << std::setw(fieldWidth) << movement.str() << tracksTraversed << std::endl;
-        totalTracksTraversed += tracksTraversed;
-        movement.str(""); 
+        printStats(trackNums[i-1], trackNums[i], movement, totalTracksTraversed);
     }
     std::cout << std::endl;
     std::cout << std::setw(fieldWidth) << "Total Tracks Traversed" << totalTracksTraversed  << std::endl;
-    //3 23 65
-    //3 23 65 34 21 19 10 12 32 97 16
-
 }
 
 
@@ -97,7 +73,6 @@ void shortestSeekTimeFirst(int trackNums[], int numOfTracksEntered)
 {
     const int fieldWidth = 30;
 
-    //3 23 65 34 21 19 10 12 32 97 16
     int trackMovingTo = 0;//This will hold the track number that will be read next.
     int tracksTraversed = 0;//This is the number of tracks that were traversed to get from the current track to the next nearest track.
     int totalTracksTraversed = 0;//This will be the total number of tracks that were traversed after all the tracks have been read.
@@ -111,39 +86,35 @@ void shortestSeekTimeFirst(int trackNums[], int numOfTracksEntered)
     for(int i = 0; i < numOfTracksEntered; i++)
         tracks.push_back(trackNums[i]);
 
-
-
-    int traverseDistance = 0;//This will be updated to hold the traversal distance to the next nearest track from the current track.
     //This for loop will find the nearest track to the initial track, which is track 15.
+    //The tracksTraversed variable will be updated to hold the traversal distance to the next nearest track from the current track.
     for(int i = 0; i < tracks.size(); i++)
     {
         if(i == 0)//This will simply initialize the traverseDistance variable to hold the distance to the first track in the tracks vector. 
         {
-            traverseDistance = std::abs(INITIAL_TRACK_POSITION - tracks[i]);
+            tracksTraversed = std::abs(INITIAL_TRACK_POSITION - tracks[i]);
             indexOfTrack = i;
         }
         else//Each subsequent iteration of the for loop will update the traverseDistance variable if the tracks[i] happens to be closer to the current track.
         {
-            if(std::abs(INITIAL_TRACK_POSITION - tracks[i]) < traverseDistance)
+            if(std::abs(INITIAL_TRACK_POSITION - tracks[i]) < tracksTraversed)
             {
-                traverseDistance = std::abs(INITIAL_TRACK_POSITION - tracks[i]);
+                tracksTraversed = std::abs(INITIAL_TRACK_POSITION - tracks[i]);
                 indexOfTrack = i;//This variable will hold the index value in the tracks vector of the closest track to the current track(that being the INITIAL_TRACK_POSITION in this case.
             }
-
         }
-
-
-
     }
     trackMovingTo = tracks[indexOfTrack];//I need to update the trackMovingTo variable to hold the value of the track that I will move to next.
-    tracksTraversed = traverseDistance;//
+    tracksTraversed;// = traverseDistance;//
     
     std::ostringstream movement;//Using the stringstream allows me to easily concatenate numbers into a string.
     
     movement << "Tracks " << INITIAL_TRACK_POSITION << "-" << trackMovingTo;
     std::cout << std::setw(fieldWidth) << movement.str() << tracksTraversed << std::endl;
     totalTracksTraversed += tracksTraversed;
-    movement.str("");//Clears the underlying string the stringsteam so that I can reuse it in later on.
+    movement.str("");//Clears the underlying string of the stringsteam so that I can reuse it later on.
+
+    tracksTraversed = 0;//I will reset this value to zero to begin the next round of iterations on the tracks vector.
     
 
     int currentTrack = trackMovingTo;//The track that I am moving to is now the current track.
@@ -151,65 +122,50 @@ void shortestSeekTimeFirst(int trackNums[], int numOfTracksEntered)
 
     while(tracks.size() > 0)
     {
+        //The tracksTraversed variable will be updated to hold the traversal distance to the next nearest track from the current track.
         for(int i = 0; i < tracks.size(); i++)
         {
             if(i == 0)
             {
-                traverseDistance = std::abs(currentTrack - tracks[i]);
+                tracksTraversed = std::abs(currentTrack - tracks[i]);
                 indexOfTrack = i;
             }
             else
             {
-                if(std::abs(currentTrack - tracks[i]) < traverseDistance)
+                if(std::abs(currentTrack - tracks[i]) < tracksTraversed)
                 {
-                    traverseDistance = std::abs(currentTrack - tracks[i]);
+                    tracksTraversed = std::abs(currentTrack - tracks[i]);
                     indexOfTrack = i;//This variable will hold the index value in the tracks vector of the closest track to the current track.
                 }
-
             }
-
-
         }
         trackMovingTo = tracks[indexOfTrack];//I need to update the trackMovingTo variable to hold the value of the track that I will move to next.
-        tracksTraversed = traverseDistance;
+        tracksTraversed;// = traverseDistance;
 
         movement << "Tracks " << currentTrack << "-" << trackMovingTo;
         std::cout << std::setw(fieldWidth) << movement.str() << tracksTraversed << std::endl;
         totalTracksTraversed += tracksTraversed;
         movement.str("");
-        
 
+        tracksTraversed = 0;//I will reset this value to zero to begin the next round of iterations on the tracks vector.
+        
         currentTrack = trackMovingTo;
         tracks.erase(tracks.begin()+indexOfTrack);
-
-
-
-
     } 
 
     std::cout << std::endl;
     std::cout << std::setw(fieldWidth) << "Total Tracks Traversed" << totalTracksTraversed  << std::endl;
-        
-
-
-
-
-
-
-
-
 
 }
 
 void scanLook(int trackNums[], int numOfTracksEntered)
 {
     const int fieldWidth = 30;
-    int tracksTraversed = 0;
     int totalTracksTraversed = 0;
 
 
-    std::vector<int> higherTracks;
-    std::vector<int> lowerTracks;
+    std::vector<int> higherTracks;//This vector will contain all of the tracks above track 15.
+    std::vector<int> lowerTracks;//This vector will contain all of the tracks below and including track 15(it will only include track 15 if it is entered again as input).
 
     for(int i = 0; i < numOfTracksEntered; i++)
     {
@@ -223,72 +179,54 @@ void scanLook(int trackNums[], int numOfTracksEntered)
     std::sort(higherTracks.begin(),higherTracks.end());
     std::sort(lowerTracks.begin(),lowerTracks.end());
 
-    
 
-    
-
-    std::cout << "Scan Look" << std::endl;
+    std::cout << "Scan-Look" << std::endl;
     std::cout << std::setiosflags(std::ios::left) << std::setw(fieldWidth) << "Head Movement" << "Tracks Traversed" << std::endl;
 
     std::ostringstream movement;
-    
-    movement << "Tracks " << INITIAL_TRACK_POSITION << "-" << higherTracks[0];
-    tracksTraversed = std::abs(INITIAL_TRACK_POSITION - higherTracks[0]);
-    std::cout << std::setw(fieldWidth) << movement.str() << tracksTraversed << std::endl;
-    totalTracksTraversed += tracksTraversed;
-    movement.str("");
-
-    for(int i = 1; i < higherTracks.size(); i++)
+   
+    //If the higherTracks vector isn't empty, then the for loop will print the stats for all the tracks higher than track 15.
+    if(!higherTracks.empty())
     {
-        movement << "Tracks " << higherTracks[i-1] << "-" << higherTracks[i];
-        tracksTraversed = std::abs(higherTracks[i-1] - higherTracks[i]);
-        std::cout << std::setw(fieldWidth) << movement.str() << tracksTraversed << std::endl;
-        totalTracksTraversed += tracksTraversed;
-        movement.str("");
-    }
+        printStats(INITIAL_TRACK_POSITION, higherTracks[0], movement, totalTracksTraversed);
 
-    movement << "Tracks " << higherTracks[higherTracks.size()-1] << "-" << lowerTracks[lowerTracks.size()-1];
-    tracksTraversed = std::abs(higherTracks[higherTracks.size()-1] - lowerTracks[lowerTracks.size()-1]);
-    std::cout << std::setw(fieldWidth) << movement.str() << tracksTraversed << std::endl;
-    totalTracksTraversed += tracksTraversed;
-    movement.str("");
-
-    for(int i = lowerTracks.size()-1; i >= 0; i--)
-    {
-        if(i-1 >= 0)//This keeps me from accessing an element ouside of the vector.
+        for(int i = 1; i < higherTracks.size(); i++)
         {
-            movement << "Tracks " << lowerTracks[i] << "-" << lowerTracks[i-1];
-            tracksTraversed = std::abs(lowerTracks[i] - lowerTracks[i-1]);
-            std::cout << std::setw(fieldWidth) << movement.str() << tracksTraversed << std::endl;
-            totalTracksTraversed += tracksTraversed;
-            movement.str("");
-        }        
+            printStats(higherTracks[i-1], higherTracks[i], movement, totalTracksTraversed);
+        }
+    }
+    
+    //This takes care of the traversal distance from the maximum track to the track that is closest to and below track 15.
+    if(!higherTracks.empty() && !lowerTracks.empty())
+        printStats(higherTracks[higherTracks.size()-1], lowerTracks[lowerTracks.size()-1], movement, totalTracksTraversed);
+    
+    //If the lowerTracks vector isn't empty, then the for loop will print the stats for all the tracks lower than track 15.
+    if(!lowerTracks.empty())
+    {
+        if(higherTracks.empty())//If there were no tracks higher than 15, then I'll start at track 15 and immediately move downward to lower track numbers.
+            printStats(INITIAL_TRACK_POSITION, lowerTracks[lowerTracks.size()-1], movement, totalTracksTraversed);
 
+        for(int i = lowerTracks.size()-1; i >= 0; i--)
+        {
+            if(i-1 >= 0)//This keeps me from accessing an element ouside of the vector.
+            {
+                printStats(lowerTracks[i], lowerTracks[i-1], movement, totalTracksTraversed);
+            }        
+        }
     }
         
     std::cout << std::endl;
     std::cout << std::setw(fieldWidth) << "Total Tracks Traversed" << totalTracksTraversed  << std::endl;
-
-
-
-
-
-
 }
-/*
 
-137 425 259 291 497 489 375 58 487 185 476 440 413 194 231 93 323 67 334 414 181 483 380 92 258 20 218 186 18 349 457 283 393 75 139 281 63 97 11 232 70 359 94 160 274 353 447 73 303 1 146 21 412 472 455 416 372 76 180 326 255 343 191 113 391 161 460 57 7 120 294 240 494 236 485 498 122 470 305 130 15 288 170 138 254 429 108 475 319 168 175 234 198 80 408 151 77 52 415 69
+void printStats(int currentTrack, int nextTrack, std::ostringstream &movement, int &totalTracksTraversed)
+{
+    const int fieldWidth = 30;
+    int tracksTraversed = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-   */
+    movement << "Tracks " << currentTrack << "-" << nextTrack;
+    tracksTraversed = std::abs(currentTrack - nextTrack);
+    std::cout << std::setw(fieldWidth) << movement.str() << tracksTraversed << std::endl;
+    totalTracksTraversed += tracksTraversed;
+    movement.str("");//Clears the underlying string of the stringsteam so that I can reuse it later on.
+}
