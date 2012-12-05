3cb3bbb11a238220b4728c8bcaaa57125ec72f98
Jed Dunn <dunnjed85@gmail.com>
2012-12-05 12:28:30 -0700

    Completed all the code for the assignment. Now I need to test and make necessary corrections.

---
+++ project-7a/project-7a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <signal.h>
+
+
+int main(int argc, char* argv[])
+{
+   printf("Name: Jed Dunn\nClass: CS3060\nSection: 601\n"); 
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    return 0;
+}
